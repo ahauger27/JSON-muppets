bf8ee3474307aad6dd8b842ca369b5dc24ac05b5
Austin <auhauger@gmail.com>
2026-01-16 12:03:25 -0500

    first-commit

---
+++ Muppet.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace JSON_muppets;
+
+public class Muppet
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Species { get; set; }
+    public string Performer { get; set; }
+    public string[] Appearances { get; set; }
+}
