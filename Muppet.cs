adc5b0d64aa9877a46fa5ceecae558eca221aee7
Austin <auhauger@gmail.com>
2026-01-16 12:05:58 -0500

    fixed class nullable problems

--- Muppet.cs
+++ Muppet.cs
@@ -5,9 +5,9 @@ namespace JSON_muppets;
 public class Muppet
 {
     public int Id { get; set; }
-    public string FirstName { get; set; }
-    public string LastName { get; set; }
-    public string Species { get; set; }
-    public string Performer { get; set; }
-    public string[] Appearances { get; set; }
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+    public string? Species { get; set; }
+    public string? Performer { get; set; }
+    public string[]? Appearances { get; set; }
 }
