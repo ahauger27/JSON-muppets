23d2998281030d9e0a35cecfb0e8f744597d7c9c
Austin <auhauger@gmail.com>
2026-01-18 10:10:16 -0500

    program now prints data from JSON

--- Program.cs
+++ Program.cs
@@ -1,5 +1,22 @@
 ﻿using System.IO;
 using System.Text.Json;
+using JSON_muppets;
+
+string muppetJsonData = File.ReadAllText("muppets.json");
+
+var muppets = JsonSerializer.Deserialize<List<Muppet>>(muppetJsonData);
+
+if (muppets != null)
+{
+    foreach (Muppet muppet in muppets)
+    {
+        Console.WriteLine($"{Environment.NewLine}Name: {muppet.FirstName} {muppet.LastName}{Environment.NewLine}Species: {muppet.Species}{Environment.NewLine}Performer: {muppet.Performer}");
+        
+        if (muppet.Appearances != null)
+        {
+            Console.WriteLine($"Appearances: {string.Join(", ", muppet.Appearances)}");
+        }
+    }
+}
 
 
-string jsonName = "muppets.json";
